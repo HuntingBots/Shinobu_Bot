2f6d3752e986c1a4c3fe420fe6491dd534b82e51
Tamim Zaman <86077807+TamimZaman99@users.noreply.github.com>
2021-07-02 09:05:33 +0600

    Update userinfo.py

--- GabiBraunRobot/modules/userinfo.py
+++ GabiBraunRobot/modules/userinfo.py
@@ -291,7 +291,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/EldianGabi_UpdateChannel/3">?</a>]'.format(
+        text += ' [<a href="https://t.me/Shinobu_Update_Channel/6">?</a>]'.format(
             bot.username)
 
     try:
@@ -514,7 +514,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/Gabi_Support_Log/9)
+ Come and see [HP System explained](https://t.me/Shinobu_Update_Channel/8)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
