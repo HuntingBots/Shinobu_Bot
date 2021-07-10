18151f592cf24f0af329d5625aa9b25da6dcc50f
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-10 16:12:11 +0530

    Update pluginshelper.py

--- GabiBraunRobot/pyrogram/pluginshelper.py
+++ GabiBraunRobot/pyrogram/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from AsunaRobot  import OWNER_ID, SUPPORT_CHAT
-from AsunaRobot import pbot
+from GabiBraunRobot  import OWNER_ID, SUPPORT_CHAT
+from GabiBraunRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
